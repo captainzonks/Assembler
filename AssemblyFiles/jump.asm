813a290df9158543cc12645155588abd55d5e955
Matthew Barham <zonks.zonks@gmail.com>
2022-04-04 18:13:16 -0600

    improve: updated jump.asm with loadi and storei instructions

--- AssemblyFiles/jump.asm
+++ AssemblyFiles/jump.asm
@@ -1,18 +1,23 @@
-JNS Label
+JNS FirstJump
 OUTPUT
 HALT
-Label, DEC 0
+FirstJump, DEC 0    // DEC 0 is where the return address is stored
 LOAD X
 SUB Y
+STOREI StoragePtr
+OUTPUT
+JNS SecondJump
+JMPI FirstJump
+SecondJump, DEC 0
+LOADI PTR
+ADD W
 STORE Z
-JNS Label2
-JMPI Label
-Label2, DEC 0
-ADD T
-STORE Z
-JMPI Label2
-T, DEC 13
-X, DEC 2
+JMPI SecondJump
+PTR, DEC 20 // pointer to Q
+StoragePtr, DEC 19
+W, DEC 13   // value variables
+X, DEC 4
 Y, DEC 2
-Z, DEC 0
+Z, DEC 0    // our storage variable
+Q, DEC 5
 END
